6749c2f5f73a8c320d60b65fd12ec4aeb0307ba6
SanderMiller <amiller@olin.edu>
2019-11-10 23:13:40 -0500

    Added image resizing

--- WorkingWithMatrices.py
+++ WorkingWithMatrices.py
@@ -1,40 +1,45 @@
 import numpy as np 
 import cv2 as cv
 import matplotlib.pyplot as plt
+import imutils
+import math
 
-bgrImage = cv.imread('Glasses.PNG')
-rgbImage = cv.cvtColor(bgrImage, cv.COLOR_BGR2RGB)
-grayImage = cv.cvtColor(bgrImage, cv.COLOR_BGR2GRAY)
-
-
-coordinates = []
-pxSize = 6
-thresh = 80
 
 def resizeImage(image, maxWidth, maxHeight, pixelSize):
-    if image.shape[1]*pixelSize <= maxWidth and image.shape[0]*pixelSize <= maxHeight:
-        return image
 
+    print('Original dimensions: {}'.format(image.shape))
+    targetWidth = math.floor(maxWidth/pixelSize)
+    targetHeight = math.floor(maxHeight/pixelSize)
 
-def binarizeImage(grayscaleImage, thresh):
-    binaryImage = grayscaleImage
-    for i in range(binaryImage.shape[0]):
-        for j in range(binaryImage.shape[1]):
-            if binaryImage[i, j] >= thresh:
-                binaryImage[i, j] = 1
-            else:
-                binaryImage[i, j] = 0
-    return binaryImage
+    if image.shape[1]>image.shape[0]:
+        image = imutils.rotate_bound(image, 270)
+        print('flipped')
+    
+    imageWidth = image.shape[1]
+    imageHeight = image.shape[0]
 
+    if imageWidth>targetWidth or imageHeight>targetHeight:
+        interpolationMethod = cv.INTER_AREA
+    
+    else:
+        interpolationMethod = cv.INTER_CUBIC
+        #interpolationMethod = cv.INTER_LINEAR    worse but faster
 
+    if imageWidth/targetWidth >= imageHeight/targetHeight:
+        print('Scaling based on width')
+        newWidth = targetWidth
+        newHeight = int((targetWidth/imageWidth)*imageHeight)
+       
+    else:
+        print('Scaling based on height')
+        newWidth = int((targetHeight/imageHeight)*imageWidth)
+        newHeight = targetHeight
+        
+    dim = (newWidth, newHeight)
+    image = cv.resize(image, dim, interpolation  = interpolationMethod)
+    print('Final dimensions: {}'.format(image.shape))
+    return image
 
-def getCoords(binaryImage, pixelSize):
-    coordinates = []
-    for i in range(binaryImage.shape[0]):
-        for j in range(binaryImage.shape[1]):
-            if binaryImage[i, j] == 255:
-                coordinates.append([j*pixelSize, i*pixelSize])
-    return coordinates
 
 
 def grayScaleFloydSteinberg(grayscaleImage):
@@ -49,6 +54,8 @@ def grayScaleFloydSteinberg(grayscaleImage):
             grayscaleImage[y+1, x] = grayscaleImage[y+1, x] + (5/16)*error
             grayscaleImage[y+1, x+1] = grayscaleImage[y+1, x+1] + (1/16)*error
     return grayscaleImage
+
+
 
 def colorScaleFloydSteinberg(RGBImage):
     for y in range(RGBImage.shape[0]-1):
@@ -65,16 +72,32 @@ def colorScaleFloydSteinberg(RGBImage):
     RGBImage = np.delete(RGBImage, RGBImage.shape[0]-1, 0)  #Delete the bottom row
     RGBImage = np.delete(RGBImage, RGBImage.shape[1]-1, 1)  #Delete the left most column
     return RGBImage
-        
 
-#binaryImage = binarizeImage(BWImage, 80)
+
+
+def getCoords(binaryImage, pixelSize):
+    coordinates = []
+    for i in range(binaryImage.shape[0]):
+        for j in range(binaryImage.shape[1]):
+            if binaryImage[i, j] == 255:
+                coordinates.append([j*pixelSize, i*pixelSize])
+    coordinates.append([0,0]) #Go Back Home when complete
+    return coordinates
+
+
+
+
+bgrImage = cv.imread('Google.png')
+rgbImage = cv.cvtColor(bgrImage, cv.COLOR_BGR2RGB)
+rgbImage = resizeImage(rgbImage,85,110,1)
+grayImage = cv.cvtColor(rgbImage, cv.COLOR_RGB2GRAY)
 
 ditheredImageGray = grayScaleFloydSteinberg(grayImage)
 ditheredImageRGB = colorScaleFloydSteinberg(rgbImage)
 #coords = getCoords(ditheredImage, 1)
 #print(len(coords))
 rgbImage = cv.cvtColor(bgrImage, cv.COLOR_BGR2RGB)
-
+rgbImage = resizeImage(rgbImage,850,1100,1)
 plt.subplot(131),plt.imshow(rgbImage)
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(132),plt.imshow(ditheredImageRGB)
